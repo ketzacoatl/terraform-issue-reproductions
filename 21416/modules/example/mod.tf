6c59af2c7782f81bb543c573ab5b8b7143498495
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2020-10-22 16:26:05 +0100

    21416: update repro to actually use both providers, and it still passes
    my "it still fails" before was incorrect!

--- 21416/modules/example/mod.tf
+++ 21416/modules/example/mod.tf
@@ -1,8 +1,17 @@
 resource "aws_vpc" "tf-21416-us-west-2" {
   provider   = aws.us-west-2
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.201.0.0/16"
 
   tags = {
-    Name = "terraform-gh-21416"
+    Name = "terraform-gh-21416-us-west-2"
+  }
+}
+
+resource "aws_vpc" "tf-21416-main" {
+  provider   = aws
+  cidr_block = "10.101.0.0/16"
+
+  tags = {
+    Name = "terraform-gh-21416-main"
   }
 }
